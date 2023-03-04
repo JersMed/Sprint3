a57d1a4979f535126e63aae7c062579d58ba796a
JersMed <jersmed@gmail.com>
2023-03-04 22:21:27 +0100

    Agregando validaciones

--- js/checkout.js
+++ js/checkout.js
@@ -5,24 +5,55 @@ function validate() {
 	// Get the input fields
 	var fName = document.getElementById("fName");
 	var fEmail = document.getElementById("fEmail");
+	var fAddress = document.getElementById("fAddress");
+	var fLastN = document.getElementById("fLastN");
+	var fPassword = document.getElementById("fPassword");
+	var fPhone = document.getElementById("fPhone");
 
-	// Get the error elements
-	var errorName = document.getElementById("errorName");
-	var errorEmail = document.getElementById("errorEmail");  
+	// // Get the error elements
+	// var errorName = document.getElementById("errorName");
+	// var errorEmail = document.getElementById("errorEmail");  
 	
 	// Validate fields entered by the user: name, phone, password, and email
-	if(fName.value == ""){
-		error++;
+	if(fName.value === "" || fName.value.length < 3 || fName.value.match(/^[a-zA-Z]+$/)){
+		$("#fName").addClass("alert-warning").text("Please enter a valid name");
+		//error++;
 	}
 
-	if(fEmail.value == ""){
-		error++;
+	if(fEmail.value === "" || fEmail.value.length < 3 || fEmail.value.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/
+	)){
+		$("#fEmail").addClass("alert-warning").text("Please enter a valid email address");
+		// error++;
 	}
-	 
-	if(error>0){
-		alert("Error");
-	}else{
-		alert("OK");
+
+	if(fAddress.value === "" || fAddress.value.length < 3){
+		$("#fAddress").addClass("alert-warning").text("Please enter a valid address");
+		// error++;
 	}
+
+	if(fLastN.value === "" || fLastN.value.length < 3 || fLastN.value.match(/^[a-zA-Z]+$/)){
+		$("#fLastN").addClass("alert-warning").text("Please enter a valid Last Name");
+		//error++;
+	}
+	
+	if(fPassword.value === "" || fPassword.value.length < 3 || fPassword.value.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+	)){
+		$("#fPassword").addClass("alert-warning").text("Please enter Password with at least one number or letter");
+		//error++;
+	}
+
+	if(fPhone.value === "" || fPhone.value.length < 3 || isNaN(fPhone.value)){
+		$("#fPhone").addClass("alert-warning").text("Please enter a valid Phone Number");
+		//error++;
+	}
+
+
+
+
+	// if(error>0){
+	// 	alert("Error");
+	// }else{
+	// 	alert("OK");
+	// }
 
 }
